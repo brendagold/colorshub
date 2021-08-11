217c368c663f7737b76bac9cc6aee06253578bd5
brendagold <petersbrenda47@gmail.com>
2021-08-11 05:40:42 -0400

    initial commit

---
+++ src/palette.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Palette extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div className="palette">
+                <div className="palette-colors"></div>
+                
+            </div>
+         );
+    }
+}
+ 
+export default Palette;
