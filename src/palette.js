49626d54b28ef18076f292edb4ed264bad1daffb
brendagold <petersbrenda47@gmail.com>
2021-08-12 17:48:44 -0400

    Added navbar

--- src/palette.js
+++ src/palette.js
@@ -1,16 +1,30 @@
 import React, { Component } from "react";
 import ColorBox from "./colorBox";
-import './palette.css'
+import NavBar from "./navbar";
+import "./palette.css";
+
 
 class Palette extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { level: 500 };
+    this.changeLevel = this.changeLevel.bind(this);
+  }
+
+  changeLevel(level) {
+    this.setState({ level });
+  }
   render() {
-      let colors = this.props.colors
-    const colorBoxes = colors.map((color) => 
-      <ColorBox background={color.color} name={color.name} />
-    );
-    
+    const {colors} = this.props.palette;
+    const {level} = this.state;
+    const colorBoxes = colors[level].map((color) => (
+      <ColorBox background={color.hex} name={color.name} />
+    ));
+
     return (
       <div className="palette">
+       <NavBar level={level} changeLevel={this.changeLevel} />
+        
         <div className="palette-colors">{colorBoxes}</div>
       </div>
     );
