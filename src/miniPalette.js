5f2367a5b3beb299763a7fde6097bc1d82682885
brendagold <petersbrenda47@gmail.com>
2021-08-17 02:46:42 -0400

    added mini palette

--- src/miniPalette.js
+++ src/miniPalette.js
@@ -1,20 +1,74 @@
-import React from 'react';
-import { withStyles } from '@material-ui/styles';
+import React from "react";
+import { withStyles } from "@material-ui/styles";
 
 const styles = {
-    main: {
-        backgroundColor: "purple",
-        border: "3px solid teal"
-    }
-}
+  root: {
+    backgroundColor: "white",
+    border: "3px solid teal",
+    borderRadius: "5px",
+    padding: "0.5rem",
+    position: "relative",
+    overflow: "hidden",
+    "&:hover": {
+      cursor: "pointer",
+    },
+  },
+
+  colors: {
+    backgroundColor: "#dae1e4",
+    height: "150px",
+    width: "100%",
+    overflow: "hidden",
+    borderRadius: "5px"
+  },
+
+  title: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: "0",
+    color: "black",
+    paddingTop: "0.5rem",
+    fontSize: "1rem",
+    position: "relative",
+  },
+  emoji: {
+    marginLeft: "0.5rem",
+    fontSize: "1.5rem",
+  },
+
+  miniColor: {
+    height: "25%",
+    width: "20%",
+    display: "inline-block",
+    margin: "0 auto",
+    position: "relative",
+    marginBottom: "-3.5px"
+  }
+};
 
 const MiniPalette = (props) => {
-    const {classes} = props
-    return ( 
-        <div className={classes.main}>
-            <h1>Mini Palerre</h1>
-        </div>
-     );
-}
- 
+  const { classes, emoji, paletteName, colors } = props;
+
+  const miniColorBoxes = colors.map((color) => (
+    <div
+      className={classes.miniColor}
+      style={{ backgroundColor: color.color }}
+      key={color.name}
+    ></div>
+  ));
+  return (
+    <div className={classes.root}>
+      <div className={classes.colors}>
+        {miniColorBoxes}
+      </div>
+      <h5 className={classes.title}>
+        {" "}
+        {paletteName}
+        <span className={classes.emoji}>{emoji}</span>
+      </h5>
+    </div>
+  );
+};
+
 export default withStyles(styles)(MiniPalette);
