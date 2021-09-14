b56fafd8b967ee749f18cafbbb304b06dd971796
brendagold <petersbrenda47@gmail.com>
2021-09-14 15:39:21 -0400

    added color picker

--- src/newPaletteForm.js
+++ src/newPaletteForm.js
@@ -1,12 +1,159 @@
-import React, { Component } from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
+import IconButton from "@material-ui/core/IconButton";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import MailIcon from "@material-ui/icons/Mail";
+import MenuIcon from "@material-ui/icons/Menu";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+import { ChevronLeft } from "@material-ui/icons";
+import {ChromePicker} from "react-color"
+import { Button } from "@material-ui/core";
 
+const drawerWidth = 400;
+
+const styles = (theme) => ({
+  root: {
+    display: "flex",
+  },
+  appBar: {
+      transition: theme.transitions.create(["margin", "width"], {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.leavingScreen
+      })
+  },
+
+  appBarShift: {
+      width: `calc(100% - ${drawerWidth}px)`, marginLeft: drawerWidth, transition: theme.transitions.create(["margin", "width"], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  menuButton: {
+      marginLeft: 12,
+      marginRight: 20
+  },
+  hide: {
+      display: "none"
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    height: "100vh",
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    display: "flex",
+    alignItems: "center",
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    width: "100%",
+    padding: "0 8px",
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end",
+  },
+  content: {
+    flexGrow: 1,
+    height: "calc(100vh - 64px)",
+    padding: 0,
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+});
 
 class NewPaletteForm extends React.Component {
-    render() { 
-        return <div>
-            <h1>NEW PALETTE FORM!!!</h1>
-        </div>;
-    }
+  state = {
+    open: true,
+  };
+
+  handleDrawerOpen = () => {
+    this.setState({ open: true });
+  };
+  handleDrawerClose = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    const { classes, theme } = this.props;
+    const { open, colors } = this.state;
+    return (
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          className={classNames(classes.appBar, {
+            [classes.appBarShift]: open,
+          })}
+        >
+          <Toolbar disableGutters={!open}>
+            <IconButton
+              color="inherit"
+              aria-label="Open drawer"
+              onClick={this.handleDrawerOpen}
+              className={classNames(classes.menuButton, open && classes.hide)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" noWrap>
+              Persistent Drawer
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          className={classes.drawer}
+          variant="persistent"
+          anchor="left"
+          open={open}
+          classes={{ paper: classes.drawerPaper }}
+        >
+          <div className={classes.drawerHeader}>
+            <IconButton onClick={this.handleDrawerClose}>
+              <ChevronLeft />
+            </IconButton>
+          </div>
+          <Divider />
+          <Typography variant="h4"> Design Your Palette </Typography>
+          <div>
+               <Button variant='contained' color='secondary'> Clear Palette</Button>
+          <Button variant='contained' color='primary'> Randor Color</Button>
+          </div>
+         
+          <ChromePicker color="purple" onChangeComplete={(newColor) => console.log(newColor)}/>
+          <Button variant='contained' color='primary'> Add Color</Button>
+        </Drawer>
+        <main
+          className={classNames(classes.content, {
+            [classes.contentShift]: open,
+          })}
+        >
+          <div className={classes.drawerHeader}></div>
+        </main>
+      </div>
+    );
+  }
 }
- 
-export default NewPaletteForm;
+
+export default withStyles(styles, { withTheme: true })(NewPaletteForm);
