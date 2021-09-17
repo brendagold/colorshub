627fee2b971cf526d5db7f9b46a088156339c40d
brendagold <petersbrenda47@gmail.com>
2021-09-17 09:45:51 -0400

    added saving to local storage

--- src/styles/PaletteFormNavStyles.js
+++ src/styles/PaletteFormNavStyles.js
@@ -5,6 +5,11 @@ const styles = (theme) => ({
   root: {
     display: "flex",
   },
+
+  hide: {
+    display: "none",
+  },
+  
   appBar: {
     transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.sharp,
